c4bd52297100ab9a430389bddb40f4df17be3391
richard <elms>
2020-09-02 21:05:41 +0200

    iOS example working well, needs a clean up

--- iosTest.js
+++ iosTest.js
@@ -2,34 +2,32 @@
 const wdio = require("webdriverio");
 const assert = require("assert");
 
-const SELECTORS = {
-    LOGIN_SCREEN: '~Login-screen'
-};
-
 const packageAttribute = 'com.applanga.applangaandroidtest:id/';
 
 
 var client
 
 async function main () {
-
-	console.log("Start Session");
 	
 	await connectClient(false)
-
-	await client.pause(1000)
-
-	await logStatus()
 
 	await enableDraftMode()
 
 	await showScreenshotMenu();
 	
-	//await selectTag("test");
+	await takeScreenshotWithTag("page-1");
 
-	await client.pause(10000)
+	await loadSecondView()
 
+	await takeScreenshotWithTag("page-2");
 
+}
+
+async function loadSecondView()
+{
+	let button = await client.$("~Open Next View")
+	button.click()
+	//await client.pause(1000)
 }
 
 async function connectClient(noReset)
@@ -51,36 +49,31 @@ async function connectClient(noReset)
 
 async function enableDraftMode() 
 {
-
-	//await client.executeScript("mobile: touchAndHold", [{x:100,y:100, duration:5 }]);
-
-await client.performActions([{
-    "type": "pointer",
-    "id": "finger1",
-    "parameters": {"pointerType": "touch"},
-    "actions": [
-        {"type": "pointerMove", "duration": 0, "x": 100, "y": 300},
-        {"type": "pointerDown", "button": 0},
-        {"type": "pause", "duration": 5000},
-        {"type": "pointerUp", "button": 0}
-    ]
-}, {
-    "type": "pointer",
-    "id": "finger2",
-    "parameters": {"pointerType": "touch"},
-    "actions": [
-        {"type": "pointerMove", "duration": 0, "x": 300, "y": 300},
-        {"type": "pointerDown", "button": 0},
-        {"type": "pause", "duration": 5000},
-        {"type": "pointerUp", "button": 0}
-    ]
-}]);
+	await client.performActions([{
+	    "type": "pointer",
+	    "id": "finger1",
+	    "parameters": {"pointerType": "touch"},
+	    "actions": [
+	        {"type": "pointerMove", "duration": 0, "x": 100, "y": 300},
+	        {"type": "pointerDown", "button": 0},
+	        {"type": "pause", "duration": 5000},
+	        {"type": "pointerUp", "button": 0}
+	    ]
+	}, {
+	    "type": "pointer",
+	    "id": "finger2",
+	    "parameters": {"pointerType": "touch"},
+	    "actions": [
+	        {"type": "pointerMove", "duration": 0, "x": 300, "y": 300},
+	        {"type": "pointerDown", "button": 0},
+	        {"type": "pause", "duration": 5000},
+	        {"type": "pointerUp", "button": 0}
+	    ]
+	}]);
 
 	await client.pause(500)
 
 	let keyInput = await client.$("~DraftModeKeyInput")
-
-	await client.pause(500);
 
     keyInput.addValue("86f8");
 
@@ -88,9 +81,9 @@ await client.performActions([{
 
 	let okButton = await client.$("~OK")
 
-	await okButton.click();
+	await okButton.click()
 
-	await client.pause(2000);
+	await client.pause(2000)
 
 	await reopenApp()
 
@@ -98,18 +91,9 @@ await client.performActions([{
 
 async function reopenApp()
 {
-	//await client.activateApp("applanga.AppiumTestIos")
 	await connectClient(true)
-	await client.pause(2000)
-
-	await logStatus()
 }
 
-async function logStatus()
-{
-	let status = await client.status()
-	log(status)
-}
 
 async function showScreenshotMenu()
 {
@@ -124,7 +108,7 @@ async function showScreenshotMenu()
         {"type": "pointerMove", "duration": 500,"origin": "pointer", "x": 0, "y": -200},
         {"type": "pointerUp", "button": 0}
     ]
-}, {
+	}, {
     "type": "pointer",
     "id": "finger2",
     "parameters": {"pointerType": "touch"},
@@ -135,21 +119,46 @@ async function showScreenshotMenu()
         {"type": "pointerMove", "duration": 500, "origin": "pointer", "x": 0, "y": -200},
         {"type": "pointerUp", "button": 0}
     ]
-}]);
+	}]);
  	
-	await client.pause(3000);
-	//await client.releaseActions();
-	logPage()
+	await client.pause(500);
 }
 
-async function selectTag(tag)
+async function takeScreenshotWithTag(tag)
 {
-	
-	await client.pause(1000)
 
-	let source = await client.getPageSource();
+	let selectTagButton = await client.$("~Applanga.SelectTag")
+
+	let selectTagButtonLocation = await selectTagButton.getLocation()
+
+	await client.touchAction({
+	  action: 'tap',
+	  x: selectTagButtonLocation.x + 5,
+	  y: selectTagButtonLocation.y + 5
+	})
+
+	await client.pause(1000);
+
+	let tagListing = await client.$("~cell_" + tag)
+
+	tagListing.click()
+
+	await client.pause(1000);
+
+	let takeScreenshotButton = await client.$("~Applanga.CaptureScreen")
+
+	let takeScreenshotButtonLocation = await takeScreenshotButton.getLocation()
+
+	await client.touchAction({
+	  action: 'tap',
+	  x: takeScreenshotButtonLocation.x + 5,
+	  y: takeScreenshotButtonLocation.y + 5
+	})
+
+	await client.pause(1000);
 
 }
+
 
 function logPage()
 {
