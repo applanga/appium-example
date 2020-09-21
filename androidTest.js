48f0fcf1d659d17e119e01ab24fa38a25123baf1
richard <elms>
2020-09-21 09:57:16 +0200

    android auto language change example done

--- androidTest.js
+++ androidTest.js
@@ -3,36 +3,42 @@ const applanga = require("applangaappiumutils");
 
 var client
 
-const opts = {
-  path: '/wd/hub',
-  port: 4723,
-  capabilities: {
-  	locale: "DE",
-  	language: "de",
-    platformName: "Android",
-    platformVersion: "9",
-    deviceName: "Pixel_3_API_28",
-    app: __dirname + "/TestingApps/AppiumTestAppAndroid/app/build/outputs/apk/debug/app-debug.apk",
-    appPackage: "com.example.appiumtestapp",
-    appActivity: "com.example.appiumtestapp.MainActivity",
-    automationName: "UiAutomator2"
-  }
-};
+var languagesToUse = ["de","en"]
+var localesToUse = ["DE","us"]
+
+
+
+function getOptions(locale,language)
+{
+	  return {
+		path: '/wd/hub',
+		port: 4723,
+		capabilities: {
+			locale: locale,
+			language: language,
+			platformName: "Android",
+			platformVersion: "9",
+			deviceName: "Pixel_3_API_28",
+			app: __dirname + "/TestingApps/AppiumTestAppAndroid/app/build/outputs/apk/debug/app-debug.apk",
+			appPackage: "com.example.appiumtestapp",
+			appActivity: "com.example.appiumtestapp.MainActivity",
+			automationName: "UiAutomator2"
+		}
+	  };
+}
 
 async function main () {
-	
-	client = await wdio.remote(opts)
-
-	await client.pause(1000)
-
-	await applanga.enableDraftModeAndroid(client,"86f8","com.example.appiumtestapp")
-
-	await applanga.takeScreenshotWithTagAndroid(client,"page-1");
-
-	await openSecondView()
-
-	await applanga.takeScreenshotWithTagAndroid(client,"page-2");
-
+	for (let i = 0; i < languagesToUse.length; i++) {
+        const language = languagesToUse[i]
+        const locale = localesToUse[i]
+		client = await wdio.remote(getOptions(locale,language))
+		await client.pause(1000)
+		await applanga.enableDraftModeAndroid(client,"86f8","com.example.appiumtestapp")
+		await applanga.takeScreenshotWithTagAndroid(client,"page-1");
+		await openSecondView()
+		await applanga.takeScreenshotWithTagAndroid(client,"page-2");
+		await client.pause(2000)
+	}
 	await client.pause(3000)
 
 }
