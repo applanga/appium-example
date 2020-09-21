bf32b82ee120c213cb06eed0a98364bfe8852846
richard <elms>
2020-09-21 10:05:56 +0200

    ios auto language switch done

--- iosTest.js
+++ iosTest.js
@@ -4,29 +4,30 @@ const applanga = require("applangaappiumutils");
 
 var client
 
+var languagesToUse = ["de","en"]
+
+
 async function main () {
-	
-	await connectClient(false)
-
-	await applanga.enableDraftModeIos(client,"86f8")
-
-	await reopenApp()
-	
-	await applanga.takeScreenshotWithTagIos(client,"page-1");
-
-	await loadSecondView()
-
-	await applanga.takeScreenshotWithTagIos(client,"page-2");
-
+	for (let i = 0; i < languagesToUse.length; i++) {
+        const language = languagesToUse[i]
+		await connectClient(false,language)
+		await applanga.enableDraftModeIos(client,"86f8")
+		await connectClient(true,language)
+		await applanga.takeScreenshotWithTagIos(client,"page-1")
+		await loadSecondView()
+		await applanga.takeScreenshotWithTagIos(client,"page-2")
+	}
 }
 
-async function connectClient(noReset)
+
+
+async function connectClient(noReset, language)
 {
 	let opts = {
 		path: '/wd/hub',
 		port: 4723,
 		capabilities: {
-			language: "en",
+			language: language,
 			platformName: "iOS",
 			platformVersion: "13.6",
 			deviceName: "iPhone 8",
@@ -38,10 +39,7 @@ async function connectClient(noReset)
 	client = await wdio.remote(opts);
 }
 
-async function reopenApp()
-{
-	await connectClient(true)
-}
+
 
 async function loadSecondView()
 {
