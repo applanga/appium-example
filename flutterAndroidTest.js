64d0e65c4db6b77218c7cc838ff2926edd3d6251
richard <elms>
2020-09-21 10:19:14 +0200

    flutter language auto switch done

--- flutterAndroidTest.js
+++ flutterAndroidTest.js
@@ -2,13 +2,16 @@ const wdio = require("webdriverio");
 const applanga = require("applangaappiumutils");
 
 var client
-
-async function connectClient(noReset)
+var languagesToUse = ["de","en"]
+var localesToUse = ["DE","us"]
+async function connectClient(noReset,language,locale)
 {
 	let opts = {
 		path: '/wd/hub',
 		port: 4723,
 		capabilities: {
+      locale: locale,
+			language: language,
       platformName: "Android",
       platformVersion: "9",
       deviceName: "Pixel_3_API_28",
@@ -22,31 +25,20 @@ async function connectClient(noReset)
 }
 
 async function main () {
-	
-	await connectClient(false)
-
-	await client.pause(1000)
-
-  await logPage()
-  
-  await applanga.enableDraftModeAndroid(client,"714b","com.applanga.applanga_flutter_test_app")
-
-  await reopenApp()
-
-	await applanga.takeScreenshotWithTagAndroid(client,"page-1");
-
-  await openSecondView()
-
-	await applanga.takeScreenshotWithTagAndroid(client,"page-2");
-
-	await client.pause(3000)
-
+  for (let i = 0; i < languagesToUse.length; i++) {
+      const language = languagesToUse[i]
+      const locale = localesToUse[i]
+      await connectClient(false,language,locale)
+      await client.pause(1000)
+      await applanga.enableDraftModeAndroid(client,"714b","com.applanga.applanga_flutter_test_app")
+      await connectClient(true,language,locale)
+      await applanga.takeScreenshotWithTagAndroid(client,"page-1");
+      await openSecondView()
+      await applanga.takeScreenshotWithTagAndroid(client,"page-2");
+      await client.pause(3000)
+  }
 }
 
-async function reopenApp()
-{
-	await connectClient(true)
-}
 
 async function openSecondView()
 {
