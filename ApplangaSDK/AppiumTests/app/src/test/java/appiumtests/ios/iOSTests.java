0fb1ac7d1880f824416dcaeb98945de027ad5d36
Leonard Arnold <leonard.arnold1@gmail.com>
2024-02-28 17:40:00 +0100

    directory restructering

--- ApplangaSDK/AppiumTests/app/src/test/java/appiumtests/ios/iOSTests.java
+++ ApplangaSDK/AppiumTests/app/src/test/java/appiumtests/ios/iOSTests.java
@@ -47,7 +47,7 @@ class iOSTests {
         // caps.setCapability("autoAcceptAlerts", "true");
         caps.setCapability("locale", "US");
         caps.setCapability("language", "en");
-        String appPath = "/xxxxxxxxxx/WeatherSample.app";
+        String appPath = "/Users/alec/projects/applanga/appium-example/ApplangaSDK/AppiumTests/trash/WeatherSample.app";
         caps.setCapability("app", appPath);
         caps.setCapability("automationName", "XCUITest");
         driver = new IOSDriver(new URL("http://127.0.0.1:4723"), caps);
